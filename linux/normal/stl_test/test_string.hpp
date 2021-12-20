f779804ff960db90a135dfbcfbce180e4354ae66
king <249834035@qq.com>
2021-12-20 21:46:03 +0800

    add test list

--- linux/normal/stl_test/test_string.hpp
+++ linux/normal/stl_test/test_string.hpp
@@ -10,11 +10,26 @@ private:
 public:
     test_string(/* args */);
     ~test_string();
+    int findNum(string str1, string str2){
+        string::size_type ret=0;
+        int num=0;
+        while(ret != string::npos){
+            ret = str1.find(str2, ret);
+            //cout << "ret=" << ret << "npos:" <<string::npos;
+            if(ret != string::npos){
+                num++;
+                ret++;
+            }
+        }
+        return num;
+    }
 };
 
 test_string::test_string(/* args */)
 {
     cout<<"test string start"<<endl;
+    string str="Hello! Nice to meet you. How do you do. you you";
+    cout<<"find num: "<<findNum(str, "you")<<endl;
 }
 
 test_string::~test_string()
