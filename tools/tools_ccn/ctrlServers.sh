12b65eecd882cdf95dd57e7ad88ed3a324360d1a
Jeremy Wong <jeremy.wong@computecoin.network>
2022-07-14 14:30:27 +0800

    change

--- tools/tools_ccn/ctrlServers.sh
+++ tools/tools_ccn/ctrlServers.sh
@@ -7,6 +7,7 @@ function copyFileToVRFTestServer(){
     ssh  root@18.163.123.3 -p 1322  -i ./xxxxxx
     scp -P 1322 -r /root/ccn/git/boost_1_66_0.tar.bz2 root@18.163.123.3:/root/jeremy
     scp -P 1322 -r /root/ccn/git/mcp/build/mcp root@18.163.123.3:/root/ccn/git/mcp/build/
+    scp -P 1322 -r /root/ccn/config-file root@18.163.123.3:/root/ccn/
 }
 
 
