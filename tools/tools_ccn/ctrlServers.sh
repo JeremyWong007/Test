4880f2823b19465f0f2caaff45fc46a7acfe4b3d
Jeremy Wong <jeremy.wong@computecoin.network>
2022-07-27 15:05:32 +0800

    change to new account in mini

--- tools/tools_ccn/ctrlServers.sh
+++ tools/tools_ccn/ctrlServers.sh
@@ -7,7 +7,10 @@ function copyFileToVRFTestServer(){
     ssh  root@18.163.123.3 -p 1322  -i ./xxxxxx
     scp -P 1322 -r /root/ccn/git/boost_1_66_0.tar.bz2 root@18.163.123.3:/root/jeremy
     scp -P 1322 -r /root/ccn/git/mcp/build/mcp root@18.163.123.3:/root/ccn/git/mcp/build/
+    scp -P 1322 -r /root/ccn/git/mcp/build/mcp.tgz root@18.163.123.3:/root/ccn/git/mcp/build/
     scp -P 1322 -r /root/ccn/config-file root@18.163.123.3:/root/ccn/
+    scp -P 1322 -r /root/ccn/localnet/mini4/1/nodekey root@18.163.123.3:/root/ccn/localnet/mini4/1/
+    scp -P 1322 -r /root/ccn/localnet/testmode/* root@18.163.123.3:/root/ccn/localnet/testmode/
 }
 
 
--- tools/tools_ccn/miniStart.sh
+++ tools/tools_ccn/miniStart.sh
@@ -5,5 +5,5 @@ getApplicationName $*
 
 pkillAndWait mcp
 echo "restart mcp"
-#nohup ./mcp --daemon --network=3 --data_path=/root/ccn/mcp-mini --witness_account=/root/ccn/mcp-mini/0x1144B522F45265C2DFDBAEE8E324719E63A1694C.json --password=12345678 >> /dev/null 2>&1 &
-nohup ${APPLICATION_NAME} --daemon --rpc --rpc_control --network=4 --data_path=/root/ccn/localnet/mcp-mini --witness --witness_account=/root/ccn/config-file/config-file-mini/0x1144B522F45265C2DFDBAEE8E324719E63A1694C.json --password=12345678 >> /dev/null 2>&1 &
+
+nohup ${APPLICATION_NAME} --daemon --rpc --rpc_control --network=4 --data_path=/root/ccn/localnet/mcp-mini --rpc_addr=0.0.0.0 --rpc_port=8765  --witness --witness_account=/root/ccn/config-file/config-file-new/0x1144B522F45265C2DFDBAEE8E324719E63A1694C.json --password=12345678 >> /dev/null 2>&1 &
