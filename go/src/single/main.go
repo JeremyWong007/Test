19c8ee000e97db19459153f619b4bd0be52d670d
Jeremy Wong <jeremy.wong@computecoin.network>
2023-09-25 13:28:27 +0800

    add go single

---
+++ go/src/single/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Books struct {
+	title   string
+	author  string
+	subject string
+	book_id int
+}
+
+func main() {
+	//fmt.Print("Hello go!")
+	// a := 12
+	// b := 13
+	str := "Hello go2\n"
+	fmt.Print(str)
+	fmt.Printf(str)
+
+	b := Books{"Go 语言", "www.runoob.com", "Go 语言教程", 6495407}
+	fmt.Println(b)
+}
