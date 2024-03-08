09f2f8ddc8afcf11bf4e48b4b57456cf1ac2c14f
wyj <249834035@qq.com>
2024-03-08 13:24:57 +0800

    add gitLinux.sh

---
+++ tools/tools_gold/src/gitver.h
@@ -0,0 +1,15 @@
+#ifndef __GIT_VER_H
+#define __GIT_VER_H
+
+
+#define GIT_VER    "R"
+#define GIT_VERNUM "135"
+#define GIT_SHA1   "21870efa165b152e3fe897ae4d0c0c50275ae1f1"
+#define GIT_DATE   "Thu Feb 22 10:12:56 2024"
+#define GIT_BRANCH "master"
+#define GIT_REMOTE "git@github.com:JeremyWong007/Test.git"
+#define GIT_REMOTE_SHA1   "21870efa165b152e3fe897ae4d0c0c50275ae1f1"
+#define COMPILE_USER      "wangyuanjing"
+
+
+#endif
